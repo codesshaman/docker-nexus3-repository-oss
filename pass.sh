8e6dd7dda35875ed43051ed4a728f2e84f6d777d
codesshaman <nicksemail@yandex.ru>
2023-03-16 16:47:36 +0300

    line break after old pass

--- pass.sh
+++ pass.sh
@@ -5,6 +5,7 @@ GIT_PATH="$(grep "NEW_USER_PASS" .env | sed -r 's/.{,14}//')"
 if [ -f "nexus-data/admin.password" ]; then
     echo "Your first admin password:"
     cat nexus-data/admin.password
+    echo " "
 fi
 
 echo "Your changed admin password: "
